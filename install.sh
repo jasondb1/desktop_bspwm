281683662d6714739483ba6bb6482313c1491932
jasondb <jason@teamdeboer.ca>
2022-09-27 08:42:43 -0600

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+cp .propfile ~/
+cp .vimrc ~/
+cp .Xnord ~/
+cp .Xresources ~/
+cp .Xauthority ~/
+cp .viminfo ~/
+cp .bashrc ~/
+cp -r icons ~/.local/share/icons
+cp -r .themes ~/
+cp -r dotfiles/* ~/.config/
+cp -r bin ~/
+cp -r Pictures ~/
